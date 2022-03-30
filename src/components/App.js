73f51907d7a41cdc878a91fd7447611691d33e3c
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-03-29 21:24:28 -0400

    Basics are done
    - Got basic React things done like adding deleting, handling todos that are done, etc

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,8 @@
-import Controls from './Controls'
-import TodoList from './TodoList'
-import Container from 'react-bootstrap/Container'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
+import TodoList from './TodoList'
 
 function App() {
-  return (
-    <Container>
-      <Controls />
-      <TodoList />
-    </Container>
-  );
+  return <TodoList />
 }
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,36 @@
 import { useEffect, useState } from "react"
 import ListGroup from "react-bootstrap/ListGroup"
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Container from "react-bootstrap/Container"
 import Todo from '../components/Todo'
 
-const data = [
+let data = [
     { id: 1, title: "Todo 1", desc: "Todo 1 desc", isDone: false },
     { id: 2, title: "Todo 2", desc: "Todo 1 desc", isDone: true },
-    { id: 3, title: "Todo 3", desc: "Todo 1 desc", isDone: false }
+    { id: 3, title: "Todo 3", desc: "Todo 1 desc", isDone: false },
+    { id: 4, title: "Todo 4", desc: "Todo 1 desc", isDone: false },
+    { id: 5, title: "Todo 5", desc: "Todo 1 desc", isDone: true },
+    { id: 6, title: "Todo 6", desc: "Todo 1 desc", isDone: false }
 ]
 
 function TodoList() {
+    const [add, setAdd] = useState(false)
+    const [title, setTitle] = useState('')
     const [todos, setTodos] = useState(data)
 
     useEffect(() => {
-        console.log("Changed")
-    }, [todos])
+        console.log(`Todo Len ${todos.length}`)
+        setAdd(false)
+    }, [add, todos])
+
+    function handleAdd() {
+        setAdd(true)
+        const lastId = todos[todos.length - 1].id
+        todos.push({id: lastId + 1, title: title, isDone: false})
+        setTodos(todos)
+        setTitle('')
+    }
 
     function handleDelete(id) {
         const newArr = todos.filter(todo => todo.id !== id)
@@ -25,16 +42,29 @@ function TodoList() {
         const newArr = todos
         newArr[todoIndex] = { ...newArr[todoIndex], isDone: !newArr[todoIndex].isDone}
         setTodos(newArr)
+        return newArr[todoIndex].isDone
+    }
+
+    function handleIsDoneStr(id) {
+        const todoIndex = todos.findIndex(todo => todo.id === id)
+        console.log(todos[todoIndex].isDone)
+        return todos[todoIndex].isDone
     }
     
     return (
-        <ListGroup variant="flush" className="w-50 m-auto pt-5">
-            {
-                todos.map(todo => 
-                    <Todo key={todo.id} todo={todo} deleteFun={handleDelete} isDoneFun={handleIsDone}/>
-                )
-            }
-        </ListGroup>
+        <Container>
+            <Container className="d-flex justify-content-center mt-5" fluid>
+                <Form className="d-flex align-items-center">
+                    <Form.Control className="me-2" type="text" placeholder="Add Todo..." onChange={(e) => setTitle(e.target.value)} />
+                    <Button variant="primary" onClick={handleAdd}>Add</Button>
+                </Form>
+            </Container>
+            <ListGroup variant="flush" className="w-50 m-auto pt-5">
+                {
+                    todos.map(todo => <Todo key={todo.id} todo={todo} deleteFun={handleDelete} isDoneFun={handleIsDone} isDoneStrFun={handleIsDoneStr}/>)
+                }
+            </ListGroup>
+        </Container>
     )
 }
 
