03e42eca95d11be34c44e284dfdce683d434df5d
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-04-02 22:12:53 -0400

    Add, delete, isDone
    - Made Add work
    - Made Delete work
    - Made isDone work
    - Made  changes to the getTodos() functions. the onSnaphots() functions was not working nicely with adding, removing or updates the todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,16 +2,14 @@ import { useState } from "react"
 import ListGroup from "react-bootstrap/ListGroup"
 import Button from "react-bootstrap/Button"
 
-function Todo({ todo, deleteFun, isDoneFun, isDoneStrFun }) {
-    const [isDoneStr, setIsDoneStr] = useState(todo.isDone ? "Done" : "Not Done")
-    
+function Todo({ todo, deleteFun, isDoneFun }) {
     return (
         <ListGroup.Item key={ todo.id } className="d-flex align-items-center justify-content-between" >
             <div>
                 <Button onClick={ () => deleteFun(todo.id) } className="btn-close m-2"></Button>
                 { todo.title }
             </div>
-            <Button onClick={ () => {isDoneFun(todo.id); setIsDoneStr(isDoneStrFun(todo.id) ? "Done" : "Not Done")}}>{ isDoneStr }</Button>
+            <Button onClick={ () => isDoneFun(todo.id) }>{ todo.isDone ? "Done" : "Not Done" }</Button>
         </ListGroup.Item>
     )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,66 +5,69 @@ import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Container from "react-bootstrap/Container"
 import Todo from '../components/Todo'
-import { addDoc, collection, deleteDoc, getDoc, where, query, onSnapshot, Timestamp, doc, documentId, getDocs } from 'firebase/firestore'
+import { addDoc, collection, deleteDoc, where, query, doc, getDocs, updateDoc } from 'firebase/firestore'
 
 import { db } from '../firebase.config'
 
 
 function TodoList() {
     const collectionRef = collection(db, 'todo')
-    const [add, setAdd] = useState(false)
     const [title, setTitle] = useState('')
     const [todos, setTodos] = useState([])
 
     useEffect(() => {
         getTodos()
-    }, [add]);
+    }, [todos]);
 
-    function getTodos() {    
-        onSnapshot(collectionRef, (snapshot) =>
-            setTodos(snapshot.docs.map((doc) => doc.data()))
-        );
+    async function getTodos() {    
+        // This did not work for me. It had weird side effects on other functions suchs as add, remove and isDone
+        // onSnapshot(collectionRef, (snapshot) =>
+        //     setTodos(snapshot.docs.map((doc) => doc.data()))
+        // );
+        // TODO: Figure out why this functions is reading the firestore documents 48K times
+        await getDocs(collectionRef).then(snaps => {
+            setTodos(snaps.docs.map(doc => doc.data()))
+        })
     }
 
     async function handleAdd() {
-        // TODO: empty title text box on add
         await addDoc(collectionRef, { id: uuid(), title: title, isDone: false })
-        setAdd(true)
+        setTitle('')
     }
 
-    async function handleDelete(id) {
-        // TODO: get delete to work
-        console.log(`ID: ${id} to delete...`)
+    async function handleDelete(todoId) {
+        const q = query(collectionRef, where("id", "==", todoId));
+        
+        await getDocs(q).then(response => {
+            const docRef = response.docs[0].ref
+            deleteDoc(docRef)
+        })
     }
 
-    function handleIsDone(id) {
-        // TODO: get this to work with firebase, not with a local array
-        const todoIndex = todos.findIndex(todo => todo.id === id)
-        const newArr = todos
-        newArr[todoIndex] = { ...newArr[todoIndex], isDone: !newArr[todoIndex].isDone}
-        setTodos(newArr)
-        return newArr[todoIndex].isDone
-    }
-
-    function handleIsDoneStr(id) {
-        const docRef = documentId()
-        console.log(docRef)
-        // const todoIndex = todos.findIndex(todo => todo.id === id)
-        // console.log(todos[todoIndex].isDone)
-        // return todos[todoIndex].isDone
+    async function handleIsDone(todoId) {
+        const q = query(collectionRef, where("id", "==", todoId));
+        const docsRef = getDocs(q)
+        await docsRef.then(docs => {
+            docs.forEach(item => {
+                const docId = item.id
+                const isDone = item.data()['isDone']
+                const docRef = doc(db, `todo/${docId}`)
+                updateDoc(docRef, {isDone: !isDone})
+            })
+        })
     }
     
     return (
         <Container>
             <Container className="d-flex justify-content-center mt-5" fluid>
                 <Form className="d-flex align-items-center">
-                    <Form.Control className="me-2" type="text" placeholder="Add Todo..." onChange={(e) => setTitle(e.target.value)} />
+                    <Form.Control className="me-2" type="text" placeholder="Add Todo..." value={title} onChange={(e) => setTitle(e.target.value)} />
                     <Button variant="primary" onClick={handleAdd}>Add</Button>
                 </Form>
             </Container>
             <ListGroup variant="flush" className="w-50 m-auto pt-5">
                 {
-                    todos.map(todo => <Todo key={todo.id} todo={todo} deleteFun={handleDelete} isDoneFun={handleIsDone} isDoneStrFun={handleIsDoneStr}/>)
+                    todos.map(todo => <Todo key={todo.id} todo={todo} deleteFun={handleDelete} isDoneFun={handleIsDone}/>)
                 }
             </ListGroup>
         </Container>
